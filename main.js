084f05899e621d7b5f285e7a87821c39f7a42efe
Neil Sullivan <ac30delay@gmail.com>
2022-12-12 09:59:16 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const monthlyExpenses = [ 201.43, 189.22, 132.09,  238.85, 195.41 ]
+let totalExpense = 0
+
+for (const monthly of monthlyExpenses) {
+totalExpense += monthly
+
+	// Add the current monthly cost to the value of totalExpense
+}
+
+// Calculate your average monthly food costs
+const averageExpense = totalExpense / monthlyExpenses.length
+
+// Make sure you use backticks for multi-line string output
+console.log(averageExpense)
+console.log(totalExpense)
